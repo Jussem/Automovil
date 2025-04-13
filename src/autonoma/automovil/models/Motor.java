d6a4fe6235ed4cdac4149b6b3b1f5645cf42d59a
Juan Jose Morales Aristizabal <juanj.moralesa@autonoma.edu.co>
2025-04-12 23:30:58 -0500

    se agrego la clase 'Motor' y la excepcion 'YaEncendido'

---
+++ src/autonoma/automovil/models/Motor.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.automovil.models;
+
+/**
+ *
+ * @author juanb
+ */
+public class Motor {
+    
+}
